18fb8dc1c58b6a4093d9355070343b4b0c095d03
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2023-12-05 12:26:57 -0600

    made create route and controller, edited application controller to allow .json requests

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,10 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params[:id])
     render template: "products/show"
   end
+
+  def create
+    @product = Product.new(name: "marker", price: 1, image_url: "https://s3.amazonaws.com/images.penguinmagic.com/images/soc_products/original/63343-full.png", description: "It's like a pen, but more markery")
+    @product.save
+    render template: "products/show"
+  end
 end
