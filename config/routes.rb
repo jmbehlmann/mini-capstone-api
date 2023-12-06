6196777bcce2a1baf7766321f2e59b156f6cbb4c
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2023-12-06 11:25:37 -0600

    add pipes for update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+# show all
 get "/products" => "products#index"
+# show 1 thing by id
 get "products/:id" => "products#show"
+# create
 post "/products" => "products#create"
+# update
 patch "/products/:id" => "products#update"
+# delete
 delete "/products/:id" => "products#destroy"
 end
