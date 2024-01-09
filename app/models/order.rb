539ebe878694b24937965b7740cd8ec08c922927
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-01-09 13:54:20 -0600

    carted_product model, updated schema

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,20 +1,5 @@
 class Order < ApplicationRecord
 
   belongs_to :users
-  # def product
-  #   Product.find_by(id: [:product_id])
-  # end
-
-  # def subtotal
-  #   product.price
-  # end
-
-  # def tax
-  #   product.price * 0.09
-  # end
-
-  # def total
-  #   subtotal + tax
-  # end
 
 end
