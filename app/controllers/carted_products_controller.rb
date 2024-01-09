3b5948a63a63004825d9b3007a061fd63fb7d958
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-01-09 14:12:33 -0600

    modified carted_product create

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -2,11 +2,11 @@ class CartedProductsController < ApplicationController
 
   def create
     @carted_product = CartedProduct.new(
-      user_id: params[:user_id],
+      user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      status: params[:status],
-      order_id: params[:order_id]
+      status: "carted",
+      order_id: nil
     )
     if @carted_product.save
       render json: {message: "you added a product to your cart"}
